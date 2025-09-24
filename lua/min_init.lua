8dd8687b900042af4f86b7fb864f128c72d0610e
Eamon Burns <erburns@fcn.net>
2025-09-24 15:34:52 -0700

    fix(lua): properly detect Windows and WSL

--- lua/min_init.lua
+++ lua/min_init.lua
@@ -15,7 +15,7 @@ local lua_dir = vim.fs.dirname(vim.fs.abspath(debug.getinfo(1, "S").short_src))
 local build_dir = vim.fs.normalize(vim.fs.joinpath(lua_dir, "..", "build"))
 -- Name of executable
 local exe_name = "git-lsp"
-if vim.fn.has("win32") then
+if vim.fn.has("win32") == 1 and vim.fn.has("wsl") == 0 then
 	exe_name = exe_name .. ".exe"
 end
 local exe_path = vim.fs.joinpath(build_dir, exe_name)
